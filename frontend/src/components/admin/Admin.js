1e3a470581c3e1077d0929fbc14e921ce3c9cedd
kamilus67 <kamil.piotrowicz@hotmail.com>
2019-08-06 15:00:33 +0200

    add get dynamiccly form attribute

--- frontend/src/components/admin/Admin.js
+++ frontend/src/components/admin/Admin.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
+import * as userApi from '../../helpers/api/userApi';
+import * as formApi from '../../helpers/api/formApi';
 
 class Admin extends Component {
+    componentDidMount = async () => {
+        const usersCollection = await userApi.get();
+        const formFields = await formApi.getAttributeFieldsUrl(1);
+    }
+
     render() {
         return(
             <div>Admin</div>
